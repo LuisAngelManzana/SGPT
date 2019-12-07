255b7745c45cf1892340442355ee98f63fe5d8dc
Andreswero <andrescz2167@gmail.com>
2019-12-06 20:27:57 -0600

    va de nuez

---
+++ App/LoginITSZN/Crud/index.php
@@ -0,0 +1,69 @@
+<?php
+	include_once 'conexion.php';
+
+	$sentencia_select=$con->prepare('SELECT *FROM usuario ORDER BY id DESC');
+	$sentencia_select->execute();
+	$resultado=$sentencia_select->fetchAll();
+
+	// metodo buscar
+	if(isset($_POST['btn_buscar'])){
+		$buscar_text=$_POST['buscar'];
+		$select_buscar=$con->prepare('
+			SELECT *FROM usuario WHERE nombre_c LIKE :campo OR usuario LIKE :campo;'
+		);
+
+		$select_buscar->execute(array(
+			':campo' =>"%".$buscar_text."%"
+		));
+
+		$resultado=$select_buscar->fetchAll();
+
+	}
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Inicio</title>
+	<link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
+	<div class="contenedor">
+		<h2>GESTION DE CUENTAS</h2>
+		<div class="barra__buscador">
+			<form action="" class="formulario" method="post">
+				<input type="text" name="buscar" placeholder="buscar nombre o usuario" 
+				value="<?php if(isset($buscar_text)) echo $buscar_text; ?>" class="input__text">
+				<input type="submit" class="btn" name="btn_buscar" value="Buscar">
+				<a href="insert.php" class="btn btn__nuevo">Nuevo</a>
+			</form>
+		</div>
+		<table>
+			<tr class="head">
+				<td>Id</td>
+				<td>USUARIO</td>
+				
+				<td>NOMBRE COMPLETO</td>
+				<td>E-MAIL</td>
+				<td>TIPO</td>
+				<td colspan="2">Acción</td>
+			</tr>
+			<?php foreach($resultado as $fila):?>
+				<tr >
+					<td><?php echo $fila['id']; ?></td>
+					<td><?php echo $fila['usuario']; ?></td>
+					
+					<td><?php echo $fila['nombre_c']; ?></td>
+					<td><?php echo $fila['e_mail']; ?></td>
+					<td><?php echo $fila['tipo']; ?></td>
+					<td><a href="update.php?id=<?php echo $fila['id']; ?>"  class="btn__update" >Editar</a></td>
+					
+				</tr>
+			<?php endforeach ?>
+
+		</table>
+	</div>
+</body>
+</html>
---
+++ App/LoginITSZN/Estudiante.php
@@ -0,0 +1,30 @@
+﻿<!doctype html>
+<html lang="en">
+
+	<head>
+		<title>Gerente</title>
+	</head>
+	
+		<LINK REL=StyleSheet HREF="estilosboton/estilosboton.css" Type="text/css" MEDIA= screen>
+		  <!-- Title -->
+  <h1>Bienvenido Estudiante</h1>
+ 
+  	<?php $nombre = $_GET['Nombre']; $pass = $_GET['Pass']; $id = $_GET['id']; $tipo = $_GET['tipo'];?>
+
+  	
+
+  <!-- Buttons Start!! -->
+  <div class="buttons">
+    <a class="btn blue" HREF="Dar_de_alta_restaurantes.php?tipo=<?php echo $tipo;?>&id=<?php echo $id;?>&Nombre=<?php echo $nombre;?>&Pass=<?php echo $pass;?>"> Solicitar Titulacion</a>
+   
+   <a class="btn red" HREF="Actualizar_datosGerente.php?Nombre=<?php echo $nombre;?>&pass=<?php echo $pass;?>"> Consultar Resultados</a>
+    
+   
+  <!--<a class="btn yellow">Yellow Button</a>-->
+  </div>
+    
+  
+	</body>
+</html>
+
+		
