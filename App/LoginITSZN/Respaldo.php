683080eed7f0e81141727aa9ef5d17bb520df90f
martin medina dehum <37426445+martinmedinadehuma@users.noreply.github.com>
2019-12-16 17:09:36 -0600

    Add files via upload
    se agrego el respaldo de la base de datos

---
+++ App/LoginITSZN/Respaldo.php
@@ -0,0 +1,26 @@
+﻿<!doctype html>
+<html lang="en">
+
+	<head>
+		<title>Admin</title> 
+     <LINK REL=StyleSheet HREF="estilosboton/estilosboton.css" Type="text/css" MEDIA= screen>
+	</head>
+  
+	
+	
+		  <!-- Title -->
+  <h1>Bienvenido Administrador</h1>
+            <center> 
+<a href='http://127.0.0.1/crud.sql'>descargar</a>
+      
+
+    </center>
+  
+	</body>
+</html>
+
+
+
+
+
+
